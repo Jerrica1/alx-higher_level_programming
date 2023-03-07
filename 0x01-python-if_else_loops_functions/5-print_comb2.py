7da8b20ec1cb5baf4cbd79e9559f42e9e92578bb
Jerrica1 <jerricajackson93@outlook.com>
2023-03-07 12:18:57 -0800

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 number = 0
 while number <= 99:
     if number != 99:
-        print("{:02d}".format(number))
+        print("{:02d}, ".format(number), end='')
     else:
         print("{:02d}".format(number))
         number = number + 1

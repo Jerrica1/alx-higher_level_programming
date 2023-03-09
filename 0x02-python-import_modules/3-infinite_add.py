854ccb206acd126ca1f17234bbb94c2bc3e4e118
Jerrica1 <jerricajackson93@outlook.com>
2023-03-09 13:35:38 -0800

    3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+    import hidden_4
+    for n in dir(hidden_4):
+        if n in dir(hidden_4):
+            print(n)

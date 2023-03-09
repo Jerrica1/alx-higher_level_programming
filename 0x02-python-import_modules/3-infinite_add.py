52c994713bb7a4816f27ba87052e0289c90d9415
Jerrica1 <jerricajackson93@outlook.com>
2023-03-09 13:41:39 -0800

    3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Python the addition of all arguments."""
     import sys
-    import hidden_4
-    for n in dir(hidden_4):
-        if n in dir(hidden_4):
-            print(n)
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+    

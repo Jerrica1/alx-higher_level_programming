2ccef33f9ca7fbeab765c461377268a4614129e1
Jerrica1 <jerricajackson93@outlook.com>
2023-03-11 17:42:17 -0800

    6-print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for col in range(len(row)):
             print("{:d}".format(row[col]),
                     end=' ' if col < len(row) - 1 else '')
-        print()
+            print()

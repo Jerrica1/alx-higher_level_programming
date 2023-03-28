eca6476198992180130aae54f5b8c9284dcfb7e7
Jerrica1 <jerricajackson93@outlook.com>
2023-03-28 10:28:09 -0700

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=(1, x=0)):
     i = 0
     printed = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
             printed += 1
+            print("({})".format(my_list[i]), end="")
         except:
-            pass
-    print()
-    return printed
+            continue
+        print()
+        return printed

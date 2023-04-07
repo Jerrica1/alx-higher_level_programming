6ca03ee6e761aa59bcbeedaf97d29b8c3588057d
Jerrica1 <jerricajackson93@outlook.com>
2023-04-06 20:29:46 -0700

    Python

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
 #include <Python.h>
+#include <unicodeobject.h>
 
 /**
- * print_python_string - Prints string information
- *
- * @p: Python object
- * Return: no return
+ * print_python_sting - prints info about python strings
+ * @p: address of pyobject struct
  */
 void print_python_string(PyObject *p)
 {
-
-	PyObject *str, *repr;
-
-	(void)repr;
-	printf("[.] string object info\n");
-
+	wprintf(L"[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str"))
 	{
-		printf(" [ERROR] Invalid String Object\n");
+		wprintf(L"  [ERROR] Invalid String Object\n");
 		return;
 	}
-
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf(" type: compact ascii\n");
+		wprintf(L"  type: compact ascii\n");
 	else
-		printf(" type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf(" length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf(" value: 5s\n", PyBytes_AsString(str));
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }

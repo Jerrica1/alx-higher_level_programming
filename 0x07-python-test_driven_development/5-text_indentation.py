1923f98a930786826e6ef589203df62b14a8a6c6
Jerrica1 <jerricajackson93@outlook.com>
2023-04-07 11:07:38 -0700

    5-text_indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 """
-text identation module:
-indents text
-useful
+prints text in special format
 """
 
 
 def text_indentation(text):
-    """text must be a string, There should be no space
-    at the beginning
-    or at the end of each printed line.
+    """prints a text with 2 new lines after each of these characters: . ? :
+    Args:
+        text (str): a string
+    Raises:
+        TypeError: if text is not a string
     """
-    # text is a string, otherwise raise a TypeError
-    if isinstance(text, str) is False:
+    new = ""
+    substring = ""
+    temp = [] * 2
+    flag = 0
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    # if text is None or not isinstance(text, str) or len(text) < 0:
-    # raise TypeError('must be a string')
-    # string.replace(old, new, count)
-    # prints a text with 2 new lines
-    text = text.replace('.', '.\n\n')
-    text = text.replace('?', '?\n\n')
-    text = text.replace(':', ':\n\n')
-    # 1) cut the entire line with \n
-    # 2) cut by separator with \n
-    # to the line that was strip (for.,?)
-    # loop text and split where "\n" is)
-    # end =""no space output promp line)
-    # there should be no spaces at the beginning
-    # and the end, line strip metodo
-
-    print("\n".join([line.strip() for line in text.split("\n")]), end="")
-
-    # if isinstance(text, str) is False:
-    # raise TypeError("text must be a string")
-    # delim = ":.?"
-    # for x in delim:
-    # text = text.replace(x, "{}\n\n".format(x))
-    # text = text.replace('\\', '')
-    # text = "\n".join([line.strip() for line in text.split('\n')])
-    # print(text, end="")
+    for i in range(len(text)):
+        if i < len(text) and text[i] == '.' or text[i] == '?'\
+           or text[i] == ':':
+            if flag == 0:
+                temp = text.split(text[i], 1)
+                flag = 1
+            else:
+                temp = substring.split(text[i], 1)
+            new += temp[0].lstrip(' ') + text[i]
+            substring = temp[1]
+            print("{:s}".format(new))
+            print()
+            new = ""
+    if flag == 0:
+        text = text.lstrip(' ')
+        text = text.rstrip(' ')
+        print("{:s}".format(text), end="")
+    else:
+        substring = substring.lstrip(' ')
+        substring = substring.rstrip(' ')
+        print("{:s}".format(substring), end="")

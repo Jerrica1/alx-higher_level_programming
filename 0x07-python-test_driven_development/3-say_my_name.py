2cc3abd5346ee45ba2470b79935998b6aaf16725
Jerrica1 <jerricajackson93@outlook.com>
2023-04-07 10:36:50 -0700

    Update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Module for say_my_name method."""
+"""
+Say My Name - a function that says your name.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Method for printing first and last name.
-    Args:
-        first_name: first name string.
-        last_name: last name string.
-    Raises:
-        TypeError: If first_name or last_name are not strings.
     """
-    if not isinstance(first_name, str):
+    Says your name.
+    first_name and last_name must be strings otherwise, will raise a TypeError
+    """
+    # first_name must be an integer
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-
-    if not isinstance(last_name, str):
+    # last_name must be an integer
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
     print("My name is {:s} {:s}".format(first_name, last_name))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")

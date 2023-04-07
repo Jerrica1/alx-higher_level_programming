cc4443679b072a4f6bd3d2fdacaf0b2e936c64a4
Jerrica1 <jerricajackson93@outlook.com>
2023-04-07 10:38:38 -0700

    Update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Module for print_square method."""
+"""
+Print Square module, for printing squares with "#".
+useful for all square-based applications
+"""
 
 
 def print_square(size):
-    """Method for printing a square with # characters.
-    Args:
-        size: The int size of the square's side.
-    Raises:
-        TypeError: If size is not an int.
-        ValueError: If size is < 0.
+    """size is the size length of the square
+    size must be an integer
     """
+
+    # size must be an integer, otherwise raise a TypeError
     if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-
+        raise TypeError('size must be an integer')
+    # if size is less than 0, raise a ValueError
     if size < 0:
-        raise ValueError("size must be >= 0")
+        raise ValueError('size must be >= 0')
+    # size is equal to lado por lado
 
-    print((("#" * size + "\n") * size), end="")
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/4-print_square.txt")
+    for x in range(size):
+        print('#' * size)

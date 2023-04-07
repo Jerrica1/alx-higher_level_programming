379cda6d3ed4a1a754fed0aff667ae06ef0f7c9a
Jerrica1 <jerricajackson93@outlook.com>
2023-04-06 21:24:46 -0700

    Python

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 6-max_integer_test.py
-"""Unittest for max_integer([..])."""
+"""Unittests for max_integer([..])."""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -15,11 +14,11 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(ordered), 4)
 
     def test_unordered_list(self):
-        """Test an unordered list of integers."
+        """Test an unordered list of integers."""
         unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(max_at_beginning), 4)
+        self.assertEqual(max_integer(unordered), 4)
 
-    def test_max_at_beginning(self):
+    def test_max_at_begginning(self):
         """Test a list with a beginning max value."""
         max_at_beginning = [4, 3, 2, 1]
         self.assertEqual(max_integer(max_at_beginning), 4)
@@ -47,7 +46,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_string(self):
         """Test a string."""
         string = "Brennan"
-        self.assertEqual(max_integer(string), "name")
+        self.assertEqual(max_integer(string), 'r')
 
     def test_list_of_strings(self):
         """Test a list of strings."""

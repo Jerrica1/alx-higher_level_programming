fe91fa1c4ecee10130bc5e62b884499ea157ba05
Jerrica1 <jerricajackson93@outlook.com>
2023-04-11 11:38:16 -0700

    Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
-"""
-=============================
-Module with a class Myint
-=============================
-"""
+"""Class MyInt that inherits from int"""
 
 
 class MyInt(int):
-    """class that inherits from int"""
+    """My int class"""
 
-    def __init__(self, my_int):
-        """for initialize a value my_int"""
+    def __eq__(self, value):
+        """Myint is equal to"""
+        return self.real != value
 
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
-
-    def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
-
-    def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+    def __ne__(self, value):
+        """Myint is diferent to"""
+        return self.real == value

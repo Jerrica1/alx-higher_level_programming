d3fa07afb3b5ea2261ff2fd44208af457dd73fcd
Jerrica1 <jerricajackson93@outlook.com>
2023-04-13 09:22:48 -0700

    Update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-"""
-    function that writes a string to a text file (UTF8)
-    and returns the number of characters written:
-    Prototype: def write_file(filename="", text=""):
-    You must use the with statement
-    You don’t need to manage file permission exceptions.
-    Your function should create the file if doesn’t exist.
-    Your function should overwrite the content of the file
-    if it already exists.
-"""
+'''Module for is_kind_of_class method.'''
 
 
-def write_file(filename="", text=""):
-    """ write string to a file """
-    with open(filename, mode="w", encoding='utf-8') as file:
-        return (file.write(text))
+def is_kind_of_class(obj, a_class):
+    '''Determines if an object is a subclass of a class.'''
+    return isinstance(obj, a_class)

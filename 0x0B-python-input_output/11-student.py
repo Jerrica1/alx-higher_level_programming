a730c524f75bd3ff38ef6291d7b04bfb87154b21
Jerrica1 <jerricajackson93@outlook.com>
2023-04-13 10:20:59 -0700

    11-student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
-"""It defines a class Student."""
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Student:
-    """It represents a student."""
+class Square(Rectangle):
+    '''A subclass representing a rectangle.'''
+    def __init__(self, size):
+        '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-    def __init__(self, first_name, last_name, age):
-        """It initialize a new Student.
-        Args:
-            first_name (str): This is the first name of the student.
-            last_name (str): The last name of the student.
-            age (int): This is the age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    def area(self):
+        '''Method for area of square.'''
+        return self.__size ** 2
 
-    def to_json(self, attrs=None):
-        """This gets a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) This is the attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
-
-    def reload_from_json(self, json):
-        """This replaces all attributes of the Student.
-        Args:
-            json (dict): This is the key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+    def __str__(self):
+        '''Returns string representation of this square.'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)

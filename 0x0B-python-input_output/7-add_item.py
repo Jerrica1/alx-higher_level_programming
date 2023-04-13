9122a19fd19542889b77a82a19f25e2e0c3f9ba4
Jerrica1 <jerricajackson93@outlook.com>
2023-04-13 09:32:51 -0700

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
-import json
+'''Module for BaseGeometry class.'''
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+class BaseGeometry:
+    '''A BaseGeometry class.'''
+    def area(self):
+        '''Method to compute this area.'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Method for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

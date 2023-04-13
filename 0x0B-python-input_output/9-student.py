bb81e519ba23ba047a2f502f6b7e9e097a368e89
Jerrica1 <jerricajackson93@outlook.com>
2023-04-13 10:39:49 -0700

    9-student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first.name = first_name
+        self.last.name = last_name
+        self.age = age
 
-class Rectangle(BaseGeometry):
-    '''A subclass representing a rectangle.'''
-    def __init__(self, width, height):
-        '''Constructor.'''
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        '''Method which returns area of rectangle.'''
-        return self.__width * self.__height
-
-    def __str__(self):
-        '''String representation method.'''
-        return "[Rectangle] " + \
-            str(self.__width) + "/" + str(self.__height)
+    def to_json(self):
+        return {"first.name": self.first.name,
+                "last.name": self.last.name,
+                "age": self.age}

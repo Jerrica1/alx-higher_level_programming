800babe2d1b0ab2f8ecfca22f7fd06e7b15d8702
Jerrica1 <jerricajackson93@outlook.com>
2023-05-16 21:38:49 -0700

    12-no_cheating

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates the score of Bob to 10 in the table second_table
+-- cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+UPDATE second_table SET score = 10 WHERE name = "Bob";

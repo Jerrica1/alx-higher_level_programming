106a0db2353c64b671a1f6a5694e2106d34cd90c
Jerrica1 <jerricajackson93@outlook.com>
2023-05-16 21:34:58 -0700

    6-list_values

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- This script lists all rows of the table "first_table"
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- usage cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT * FROM first_table;

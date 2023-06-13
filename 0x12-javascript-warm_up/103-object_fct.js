a56a4801d09421e1dac584b4b8df134de9d0a031
Jerrica1 <jerricajackson93@outlook.com>
2023-06-12 22:07:01 -0700

    103-object_fct

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = () => { this.value++; };
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);

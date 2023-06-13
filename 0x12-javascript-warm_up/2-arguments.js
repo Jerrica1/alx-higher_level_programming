9fcbbce49d38b6bc5905cfaaac11d0a994d53cf6
Jerrica1 <jerricajackson93@outlook.com>
2023-06-13 06:36:08 -0700

    2-arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
-!/usr/bin/node
-if (process.argv[3]) {
-  console.log('Arguments found');
-} else if (process.argv[2]) {
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }

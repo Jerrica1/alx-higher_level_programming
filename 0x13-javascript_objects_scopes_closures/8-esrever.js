201718a04077eb7ba167ee5b92c48deddd36022a
Jerrica1 <jerricajackson93@outlook.com>
2023-06-13 06:21:46 -0700

    8-esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-  const reverse = [];
-  for (let i = 0; i < list.length; i++) {
-    reverse[list.length - i - 1] = list[i];
+  const newList = [];
+  const x = list.length;
+  for (let i = 0; i < x; i++) {
+    newList.push(list.pop());
   }
-  return reverse;
-}];
+  return newList;
+};

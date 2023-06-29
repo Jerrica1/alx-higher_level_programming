6935643695609a545236c9885db897ab9339c8df
Jerrica1 <jerricajackson93@outlook.com>
2023-06-29 15:13:33 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Take in URL, POST key:vals; Usage: ./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Displays body of response after being sent POST request
+curl -sd 'email=test@gmail.com&subject=I will always be here for PLD' "$1"

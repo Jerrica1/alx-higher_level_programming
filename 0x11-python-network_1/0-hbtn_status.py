fd64d4c5ad599db366d883ebaf6cc9b8abd09556
Jerrica1 <jerricajackson93@outlook.com>
2023-06-30 14:52:49 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""
-    HOWTO Fetch Internet Resources Using The urllib Package.
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 
-def mystatus(pagename):
-    """
-    Python script that fetches in "url" page.
-    """
-    with urllib.request.urlopen(pagename) as response:
-        html = response.read()
-        # Info! Print report status.
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode("utf-8"))
-
-
-if __name__ == '__main__':
-    mystatus('https://intranet.hbtn.io/status')
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))

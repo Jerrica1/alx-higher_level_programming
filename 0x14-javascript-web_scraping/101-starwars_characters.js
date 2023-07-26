7b8082e1b2dc2581b25661e7913341b289dd98c8
Jerrica1 <jerricajackson93@outlook.com>
2023-07-25 17:33:58 -0700

    Update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+async function get_name(character) {
+	return new Promise(resolve => {
+		request(character[1], function (error, response, body) {
+			if (error) console.log(error);
+			console.log(JSON.parse(body).name);
+		});
+	}
+	);
 }
+
+async function asdf() {
+	return request(url, function (err, response, body) {
+		if (err) console.log(err);
+		let characters = JSON.parse(body).characters;
+		for (let character of characters) {
+			await get_name(character);
+		}
+	});
+}
+
+asdf

66630ebfd7d9c9003e0c816a3b11741a071ff247
Jerrica1 <jerricajackson93@outlook.com>
2023-07-25 17:22:26 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
-// web scrapper
-
+/**
+  compute number of tasks completed by user id
+  usage: /6-completed_tasks.js <API URL>
+  */
+const myArgs = process.argv.slice(2);
 const request = require('request');
-
-request.get(process.argv[2], { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+const id_ = [];
+const result = {};
+request(myArgs[0], function (error, response, body) {
+  let i;
+  if (!error) {
+    const json_ = JSON.parse(body);
+    const len = json_.length;
+    for (i = 0; i < len; i++) {
+      if (json_[i].completed) {
+        id_.push(json_[i].userId);
       }
     }
-  });
-  console.log(tasksCompleted);
+    for (let j = 0; j < id_.length; ++j) {
+      if (!result[id_[j]]) { result[id_[j]] = 0; }
+      ++result[id_[j]];
+    }
+  }
+  console.log(result);
 });
